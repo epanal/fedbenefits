c7540ea5755dfe6c7e567c7feff1e4364c8cac74
epanal <epanal@gmail.com>
2025-03-09 23:24:07 -0700

    input fields size

--- app.py
+++ app.py
@@ -15,16 +15,6 @@ st.markdown("""
 </a>
 """, unsafe_allow_html=True)
 
-st.markdown(
-    """
-    <style>
-    div[data-baseweb="input"] {
-        max-width: 300px;  /* Adjust the width */
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
 # Add spacing after the title
 st.markdown("<br>", unsafe_allow_html=True)
 
