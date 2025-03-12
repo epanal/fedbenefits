cd9b7fc9090132c35530e02e5011ec30f9dc4330
epanal <epanal@gmail.com>
2025-03-11 20:04:36 -0700

    corrected weeks of severance to be full weeks

--- calculations.py
+++ calculations.py
@@ -82,4 +82,4 @@ def calculate_severance_pay(annual_salary, years_of_service, months_of_service, 
     # Step 10: Calculate weeks of severance (max 52)
     weeks_of_severance = min(52, adj_years_of_service)
 
-    return total_severance, basic_severance, age_adjustment, biweekly_severance, weeks_of_severance
+    return total_severance, total_severance_before_age_factor, age_adjustment, biweekly_severance, weeks_of_severance
