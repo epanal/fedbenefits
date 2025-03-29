fd1e6d67d53e929729d1d69f13bd0962be65f4d1
epanal <epanal@gmail.com>
2025-03-29 10:14:14 -0700

    added DRP input date

--- app.py
+++ app.py
@@ -148,7 +148,7 @@ def compare_severance_vs_drp():
         st.markdown(f"**Total Severance Pay Estimate:** ${severance_estimate:,.2f}", unsafe_allow_html=True)
         st.markdown(f"<p style='font-size: 16px;'><strong>Earnings Under DRP Until Sep 30, 2025:</strong> ${total_drp_earnings:,.2f} ({pay_periods_remaining} pay periods * ${biweekly_salary:,.2f})</p>", unsafe_allow_html=True)
         st.markdown(f"<p style='font-size: 16px;'><strong>Earnings During RIF Notice Period to Actual RIF:</strong> ${total_rif_earnings:,.2f} ({rif_pay_periods} pay periods * ${biweekly_salary:,.2f})</p>", unsafe_allow_html=True)
-        st.markdown(f"**Total Adjusted RIF Severance (Severance Estimate + RIF notice earnings):** ${adjusted_severance:,.2f}", unsafe_allow_html=True)
+        st.markdown(f"**Total Adjusted RIF Severance (Severance Estimate + RIF notice period earnings):** ${adjusted_severance:,.2f}", unsafe_allow_html=True)
 
         # Highlight which option is better
         if total_drp_earnings > adjusted_severance:
